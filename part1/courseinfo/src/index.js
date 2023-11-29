c6b8574dc3bd329be79a5a37a0f129420188a5d0
Raul <raulmaestre14@gmail.com>
2023-11-29 14:49:42 -0500

    1.2 Exercise

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,48 +1,82 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}
-      exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total  exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
-  )
-}
+  );
+};
 
 const Header = (props) => {
-  return(
+  return (
     <>
-    <h1>{props.course}</h1>
+      <h1>{props.course}</h1>
     </>
-  )
-}
+  );
+};
 
 const Content = (props) => {
-  return(
+  return (
+    <>
+      <Part1 part1={props.part1} exercises1={props.exercises1}/>
+      <Part2 part2={props.part2} exercises2={props.exercises2}/>
+      <Part3 part3={props.part3} exercises3={props.exercises3}/>
+    </>
+  );
+};
+
+const Part1 = (props) => {
+  return (
     <>
       <p>
         {props.part1} {props.exercises1}
       </p>
+    </>
+  );
+};
+
+const Part2 = (props) => {
+  return (
+    <>
       <p>
         {props.part2} {props.exercises2}
       </p>
+    </>
+  );
+};
+
+const Part3 = (props) => {
+  return (
+    <>
       <p>
         {props.part3} {props.exercises3}
       </p>
     </>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
@@ -51,7 +85,7 @@ const Total = (props) => {
         Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}
       </p>
     </>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
