6f0aaf7e06bee5b104d96d3cba67d41080a97e3d
Raul <raulmaestre14@gmail.com>
2023-11-30 10:46:35 -0500

    estado complejo y renderizado condicional

--- testing/src/index.js
+++ testing/src/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 //Componente con estado
+/*
 const App = () => {
   const [counter, setCounter] = useState(0);
   const operations = {
@@ -35,6 +36,48 @@ const Button = ({ handleClick, text }) => {
       <button onClick={handleClick}>{text}</button>
     </>
   );
-};
+};*/
+
+//Estado complejo
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))    
+    setLeft(left + 1)  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))   
+    setRight(right + 1)  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
